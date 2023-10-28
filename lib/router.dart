66cb34a64eb276a459aa73f9c5f8660d4b9892b2
Seungmin Jeong <sontneptm2@gmail.com>
2023-10-28 16:49:19 +0900

    update

--- lib/router.dart
+++ lib/router.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:jaljayo/feature/home/view/home_screen.dart';
 
-final routerProvier = Provider((ref) {
-  return GoRouter(
-    routes: [],
-  );
-});
+final routerProvier = Provider(
+  (ref) {
+    return GoRouter(
+      initialLocation: "/",
+      routes: [
+        GoRoute(
+          path: '/',
+          pageBuilder: (context, state) => const MaterialPage(
+            child: HomeScreen(),
+          ),
+        ),
+      ],
+    );
+  },
+);
